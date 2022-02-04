5a84e1c07d1daa186019488f5075bc747bcb4f3a
Poteto143 <59520313+Poteto143@users.noreply.github.com>
2022-02-04 15:01:23 +0900

    Delete gettoken.py

--- gettoken.py
+++
@@ -1,5 +0,0 @@
-def get(istest):
-    if istest:
-        return "NzE3NzM3ODY2ODAwNjYwNTEw.XtzXiw.PYkJwgnAAoNOqtWCQZYZexwIiJg"
-    else:
-        return "NjkxNTQ3MzU2MTAwOTUyMDk2.Xtjplg.usJe20QLZo3oqMonkE5dTlw8XXA"
